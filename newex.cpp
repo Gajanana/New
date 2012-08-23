72db025477058559f22ff8a4ba558af80813de4d
gajanana <gajukorse.hegde80@gmail.com>
2012-08-23 12:18:56 +0530

    initial project version

---
+++ newex.cpp
@@ -0,0 +1,13 @@
+// newex.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std ;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello world" ;
+	return 0;	
+}
+
